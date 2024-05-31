96030418a218f6d08c79b49292b7d76112c3d1d2
litneet64 <litneet64@gmail.com>
2024-05-31 09:34:17 -0400

    fix: remove input const assignment

--- src/tt_um_ro_puf.v
+++ src/tt_um_ro_puf.v
@@ -17,7 +17,6 @@ module tt_um_litneet64_ro_puf (
   assign rst = ~rst_n;
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uio_in = 8'b00000000;
   assign uio_out = 8'b00000000;
   assign uio_oe  = 8'b00000000;
 
